bbdcf8f9280f79854a3dca8fd1bdcaca4f8dcdcd
Alex <alexanderlcooter@gmail.com>
2021-12-04 10:31:55 -0700

    Add react-router-dom. Noticeable changes since I last used react-router-dom. Routes replaced Switch and rendering component on route is different. Review v6 changes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
+import DashboardView from "./views/dashboard/DashboardView";
 
 function App() {
   return (
     <div>
       <h1>bonsai</h1>
+
+      <Routes>
+        <Route path="/dashboard" element={<DashboardView />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/views/dashboard/DashboardView.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DashboardView = () => {
+  return (
+    <>
+      <h1>Dashboard View</h1>
+    </>
+  );
+};
+
+export default DashboardView;
