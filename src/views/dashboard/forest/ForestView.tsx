5e730381bb3e31c713f780009408b8dfa501fc49
Alex <alexanderlcooter@gmail.com>
2021-12-21 21:46:55 -0700

    Cleaned up some things and added user panel in header to conditionally render user details based on if the user is authenticated or not.

--- src/views/dashboard/forest/ForestView.tsx
+++ src/views/dashboard/forest/ForestView.tsx
@@ -1,22 +1,27 @@
 import React from "react";
 // bootstrap
-import Container from "react-bootstrap/Container";
-
+import { Container, Row, Col } from "react-bootstrap";
 // components
 import DashNav from "../../../components/menus/DashNav";
 import Logo from "../../../components/header/Logo";
 import ForestList from "../../../components/forest/ForestList";
+import UserPanel from "../../../components/menus/user/UserPanel";
 
 const ForestView = () => {
   return (
-    <Container className="w-100 d-flex main-container">
-      <Container className="w-25 bg-light">
-        <Logo />
-        <DashNav />
-      </Container>
-      <Container className="w-75">
-        <ForestList />
-      </Container>
+    <Container className="w-100 d-flex-col main-container">
+      <Row>
+        <UserPanel />
+      </Row>
+      <Row>
+        <Container className="w-25 bg-light">
+          <Logo />
+          <DashNav />
+        </Container>
+        <Container className="w-75">
+          <ForestList />
+        </Container>
+      </Row>
     </Container>
   );
 };
--- src/views/trees/PlantTreeView.tsx
+++ src/views/trees/PlantTreeView.tsx
@@ -1,22 +1,28 @@
 import React from "react";
 // bootstrap
-import Container from "react-bootstrap/Container";
+import { Container, Row, Col } from "react-bootstrap";
 
 // components
 import DashNav from "../../components/menus/DashNav";
 import Logo from "../../components/header/Logo";
 import CreateNewTree from "../../components/forest/CreateNewTree";
+import UserPanel from "../../components/menus/user/UserPanel";
 
 const PlantTreeView = () => {
   return (
-    <Container className="w-100 d-flex main-container">
-      <Container className="w-25 bg-light">
-        <Logo />
-        <DashNav />
-      </Container>
-      <Container className="w-75 bg-secondary">
-        <CreateNewTree />
-      </Container>
+    <Container className="w-100 d-flex-col main-container">
+      <Row>
+        <UserPanel />
+      </Row>
+      <Row>
+        <Container className="w-25 bg-light">
+          <Logo />
+          <DashNav />
+        </Container>
+        <Container className="w-75 bg-secondary">
+          <CreateNewTree />
+        </Container>
+      </Row>
     </Container>
   );
 };
